61962bb866417abcfaee4df6b62a20143f87efc4
koh11235813 <koh11235813@gmail.com>
2026-04-29 21:15:46 +0900

    fix: 現在の環境に修正

--- dot_config/waybar/scripts/power_menu.sh
+++ dot_config/waybar/scripts/power_menu.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-wlogout
+
+options="Lock\nSuspend\nLogout\nReboot\nShutdown"
+
+selected=$(echo -e "$options" | rofi -dmenu -i -p "Power" -theme-str 'window {width: 200px;} listview {lines: 5;}')
+
+case "$selected" in
+    Lock)     loginctl lock-session ;;
+    Suspend)  systemctl suspend ;;
+    Logout)   hyprctl dispatch exit ;;
+    Reboot)   systemctl reboot ;;
+    Shutdown) systemctl poweroff ;;
+esac
